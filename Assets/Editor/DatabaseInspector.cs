b299cc11965ec5ee17de8cfe09685fafa2278412
DarkMio <mio@zier.ws>
2016-03-07 12:35:17 +0100

    Initial Repo Commit

---
+++ Assets/Editor/DatabaseInspector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class DatabaseInspector : EditorWindow {
+    
+    string myString = "Hello World";
+	bool groupEnabled;
+	bool myBool = true;
+	float myFloat = 1.23f;
+    
+    
+    [MenuItem("Database/Overview")]
+    public static void Init() {
+        DatabaseInspector window = EditorWindow.GetWindow<DatabaseInspector>();
+        window.minSize = new Vector2(800, 400);
+        window.Show();
+    }
+    
+    void OnGUI(){
+        EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+        DisplayListArea();
+        DisplayMainArea();
+        EditorGUILayout.EndHorizontal();
+
+    }
+}
